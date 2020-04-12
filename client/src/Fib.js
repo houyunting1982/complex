c43a1a741216aff2d821fdd775f527b3e91d7df7
houyunting1982 <yhou@servicetitan.com>
2020-04-11 20:41:08 -0700

    Updated client

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -38,6 +38,7 @@ class Fib extends Component {
     };
 
     renderSeenIndexes() {
+        if (!this.state.seenIndexes) return;
         return this.state.seenIndexes.map(({ number }) => number).join(", ");
     }
 
